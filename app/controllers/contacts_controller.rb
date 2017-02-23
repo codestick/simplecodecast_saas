fe7b77180b73bcc343d78c16e478b00584d4ea1f
fvthree <kadjaski@gmail.com>
2017-02-23 23:41:24 +0000

    fix contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,13 +4,22 @@ class ContactsController < ApplicationController
     end
     
     def create
+        
+        if @contact.save
             name = params[:contact][:name]
             email = params[:contact][:email]
             body = params[:contact][:comments]
+            
             #refer to contact_mailer.rb in mailers folder
+            @contact = Contact.new(contact_params)
+            
+            flash[:success] = "Message sent."
+            redirect_to new_contact_path
             ContactMailer.contact_email(name, email, body).deliver
+        else
             flash[:danger] = "Error occured. Message has not been sent."
             redirect_to new_contact_path
+        end
     end
     
     private
