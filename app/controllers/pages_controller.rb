6cba1e68e221b81ee4a01251a162ef36d367bf01
fvthree <kadjaski@gmail.com>
2017-02-24 10:05:43 +0000

    Updated forms for stripe

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
     def home
         @basic_plan = Plan.find(1)
-        #@basic_plan.id #1
-        #@basic_plan.price #0
         @pro_plan = Plan.find(2)
     end
     
